e2f4f37132bdc59869f35870237a154ff9f7a5e6
fakorede <abiolafakorede@gmail.com>
2022-04-26 05:09:35 +0100

    Prisma and Graphql schemas

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,39 @@
 import { gql } from 'apollo-server'
 
 export const typeDefs = gql`
-  type Query {},
+  type Query {
+    accounts: [Account!]!,
+    categories: [Category!]!,
+    transactions(filters: TransactionFilters): [Transaction!]!,
+  },
+
+  type Account {
+    id:       ID!
+    name:     String!
+  },
+
+  type Category {
+    id:       ID!
+    name:     String!
+    color:     String!
+  }
+
+  type Transaction {
+    id:         ID!
+    accountId: String!
+    categoryId: String
+    reference:  String!
+    amount:     String!
+    currency:   String!
+    date:       String!
+
+    account:    Account
+    category:   Category
+  }
+
+  input TransactionFilters {
+    accountId:  String
+    startDate:  String
+    endDate:    String
+  }
 `
